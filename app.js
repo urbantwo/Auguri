b7dc9efdb296b0b72ea69dbc482ce4e621a27778
urbantwo <stefano.trevisi.95@gmail.com>
2023-08-11 17:08:10 +0200

    modifiche

--- app.js
+++ app.js
@@ -9,41 +9,5 @@ function faiAuguri() {
 
 addEventListener("DOMContentLoaded", () => {
     var pulsante = document.getElementById("pulsanteAuguri")
-    pulsante.addEventListener('click',handleSubmit)
+    pulsante.addEventListener('click',faiAuguri)
 });
-
-
-// var form = document.getElementById("form-auguri");
-    
-    async function handleSubmit(event) {
-      event.preventDefault();
-      var status = ""
-      var data = new FormData(event.target);
-      fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-      }).then(response => {
-        if (response.ok) {
-          status= "Thanks for your submission!";
-          console.log(status)
-          form.reset()
-        } else {
-          response.json().then(data => {
-            if (Object.hasOwn(data, 'errors')) {
-              status = data["errors"].map(error => error["message"]).join(", ")
-            } else {
-              status = "Oops! There was a problem submitting your form"
-            }
-          })
-
-          console.log(status)
-        }
-      }).catch(error => {
-        status = "Oops! There was a problem submitting your form"
-        console.log(status)
-      });
-    }
-    // form.addEventListener("submit", handleSubmit)
